55f8d0d2dc9c059377ecdc31b09a87d4b29b11a2
DavidJC1105 <112865062+DavidJC1105@users.noreply.github.com>
2024-03-07 17:20:13 +0000

    Add files via upload

--- LCP_Graph.py
+++ LCP_Graph.py
@@ -13,42 +13,43 @@ percentage = 0
 cred = credentials.Certificate("C:/Users/18DCummins.ACC/Downloads/lc-project-457ba-firebase-adminsdk-fwptw-549a484c99.json")
 firebase_admin.initialize_app(cred,{'databaseURL': 'https://lc-project-457ba-default-rtdb.europe-west1.firebasedatabase.app/'})
 ref = db.reference()
+#
+#ref.update({'study_time':''})
 '''
 custom_timestamps = [
     #Monday
-    {"timestamp": 1757958400, "data": {"Location": "Room 1", "Wasted_Time(minutes)": 10, "study_time(minutes)": 65}},
-    {"timestamp": 1757994800, "data": {"Location": "Room 1", "Wasted_Time(minutes)": 8, "study_time(minutes)": 30}},
+    {"timestamp": 175795840, "data": {"Location": "Room 1", "Wasted_Time(minutes)": 10, "study_time(minutes)": 65}},
+    {"timestamp": 175799480, "data": {"Location": "Room 4", "Wasted_Time(minutes)": 8, "study_time(minutes)": 30}},
     #Tuesday
-    {"timestamp": 1749064800, "data": {"Location": "Room 1", "Wasted_Time(minutes)": 15, "study_time(minutes)": 78}},
-    {"timestamp": 1749108000, "data": {"Location": "Room 1", "Wasted_Time(minutes)": 4, "study_time(minutes)": 95}},
+    {"timestamp": 174906480, "data": {"Location": "Room 3", "Wasted_Time(minutes)": 15, "study_time(minutes)": 78}},
+    {"timestamp": 174910800, "data": {"Location": "Room 2", "Wasted_Time(minutes)": 4, "study_time(minutes)": 95}},
     #Wednesday
-    {"timestamp": 1749151200, "data": {"Location": "Room 1", "Wasted_Time(minutes)": 13, "study_time(minutes)": 40}},
-    {"timestamp": 1749194400, "data": {"Location": "Room 1", "Wasted_Time(minutes)": 7, "study_time(minutes)": 37}},
+    {"timestamp": 174915120, "data": {"Location": "Room 1", "Wasted_Time(minutes)": 13, "study_time(minutes)": 40}},
+    {"timestamp": 174919440, "data": {"Location": "Room 5", "Wasted_Time(minutes)": 7, "study_time(minutes)": 37}},
     #Thursday
-    {"timestamp": 1749237600, "data": {"Location": "Room 1", "Wasted_Time(minutes)": 20, "study_time(minutes)": 92}},
-    {"timestamp": 1749280800, "data": {"Location": "Room 1", "Wasted_Time(minutes)": 16, "study_time(minutes)": 80}},
+    {"timestamp": 174923760, "data": {"Location": "Room 5", "Wasted_Time(minutes)": 20, "study_time(minutes)": 92}},
+    {"timestamp": 174928080, "data": {"Location": "Room 2", "Wasted_Time(minutes)": 16, "study_time(minutes)": 80}},
     #Friday
-    {"timestamp": 1749324000, "data": {"Location": "Room 1", "Wasted_Time(minutes)": 4, "study_time(minutes)": 25}},
-    {"timestamp": 1749367200, "data": {"Location": "Room 1", "Wasted_Time(minutes)": 14, "study_time(minutes)": 58}},
+    {"timestamp": 174932400, "data": {"Location": "Room 3", "Wasted_Time(minutes)": 4, "study_time(minutes)": 25}},
+    {"timestamp": 174936720, "data": {"Location": "Room 4", "Wasted_Time(minutes)": 14, "study_time(minutes)": 58}},
     #Saturday
-    {"timestamp": 1749410400, "data": {"Location": "Room 1", "Wasted_Time(minutes)": 6, "study_time(minutes)": 45}},
-    {"timestamp": 1749453600, "data": {"Location": "Room 1", "Wasted_Time(minutes)": 21, "study_time(minutes)": 34}},
+    {"timestamp": 174941040, "data": {"Location": "Room 1", "Wasted_Time(minutes)": 6, "study_time(minutes)": 45}},
+    {"timestamp": 174945360, "data": {"Location": "Room 5", "Wasted_Time(minutes)": 21, "study_time(minutes)": 34}},
     #Sunday
-    {"timestamp": 1749496800, "data": {"Location": "Room 1", "Wasted_Time(minutes)": 5, "study_time(minutes)": 87}},
-    {"timestamp": 1749540000, "data": {"Location": "Room 1", "Wasted_Time(minutes)": 10, "study_time(minutes)": 60}},
+    {"timestamp": 174949680, "data": {"Location": "Room 3", "Wasted_Time(minutes)": 5, "study_time(minutes)": 87}},
+    {"timestamp": 174954000, "data": {"Location": "Room 4", "Wasted_Time(minutes)": 10, "study_time(minutes)": 60}},
     # Add more custom timestamps and corresponding data as needed
 ]
-'''
+
 # Push data to Firebase
 for data_point in custom_timestamps:
     timestamp = data_point["timestamp"]
     data = data_point["data"]
     ref.child('study_time').child(str(timestamp)).update(data)
-
+'''
 
 #ref.update({'study_time':''})
 ref = db.reference().child('study_time')
-#source = input("Please input the study location: ")
 data = ref.order_by_child('Location').get()
 #print (type(data))
 wasteminutes = []
@@ -72,7 +73,7 @@ for key,value in data.items():
     if value['Location'] not in location:
         location.append(value['Location'])
 for i in range(len(location)):
-    times.append(0)
+    #times.append(0)
     wasteminutes.append(0)
     totaltime.append(0)
 for k,v in data.items():
@@ -92,8 +93,8 @@ for key,value in data.items():
 #print(location)
 #print(wasteminutes)
 dl =0
-times[dl:len(wasteminutes)] = []
-#print(times)
+#times[dl:len(wasteminutes)] = []
+print(times)
 #print(totaltime)
 lowest_value = wasteminutes[0]
 
@@ -109,7 +110,20 @@ for key, value in data.items():
     if wastedtime is not None:
         totalwaste.append(wastedtime)
 
-#print(totalwaste)       
+#print(totalwaste)
+totalwaste_average = 0
+av = 0
+index = 0
+for x in totalwaste:
+    av = av+x
+    index = index+1
+    if index == len(totalwaste):
+        break
+totalwaste_average = av/len(totalwaste)
+wasteminutes.append(totalwaste_average)
+
+
+
 totalminutes = []
 for key, value in data.items():
     totaltimes = value.get('study_time(minutes)')
@@ -117,6 +131,17 @@ for key, value in data.items():
         totalminutes.append(totaltimes)     
 
 #print(totalminutes)
+totalminutes_average = 0
+av2 = 0
+index2 = 0
+for x in totalminutes:
+    av2 = av2+x
+    index2 = index2+1
+    if index2 == len(totalminutes):
+        break
+totalminutes_average = av2/len(totalminutes)
+totaltime.append(totalminutes_average)
+location.append('Average')
 
 
 def find_lowest_time_and_waste(totalwaste, times):
@@ -236,32 +261,88 @@ formatted_times_max = []  # Initialize a list to collect formatted times
 starting_max = 0
 for eachtime in highest_times:
     eachtime = int(eachtime) - starttime_max[starting_max]  # Assuming starttime is another list
-    formatted_time_max = datetime.fromtimestamp(eachtime).strftime('%d:%H:%M:%S')
+    formatted_time_max = datetime.fromtimestamp(eachtime).strftime('%H:%M:%S')
     formatted_times_max.append(formatted_time_max)  # Collect formatted time in the list
     starting_max += 1
 
+from datetime import datetime
+
+
+def date_to_words(highest_times):
+    date_words = []
+    for timestamp in highest_times:
+        # Convert timestamp to integer
+        timestamp_int = int(timestamp)
+        
+        # Convert timestamp to datetime object
+        date_object = datetime.fromtimestamp(timestamp_int)
+    
+        # Convert datetime object to words
+        date_word = date_object.strftime('%B %d, %Y')
+        date_words.append(date_word)
+    
+    return date_words
+
+date_words = date_to_words(highest_times)
+#print(date_words)
+
+def date_to_words_2(lowest_times):
+    date_words_2 = []
+    for timestamp in lowest_times:
+        # Convert timestamp to integer
+        timestamp_int = int(timestamp)
+        
+        # Convert timestamp to datetime object
+        date_object = datetime.fromtimestamp(timestamp_int)
+    
+        # Convert datetime object to words
+        date_word = date_object.strftime('%B %d, %Y')
+        date_words_2.append(date_word)
+    
+    return date_words_2
+
+date_words_2 = date_to_words_2(lowest_times)
+#print(date_words_2)
+maxpercent = []
+a=0
+for x in maxtimes:
+    x= maxtimes[a]
+    maxi = (max_total_waste/x)*100
+    maxi_rounded = round(maxi,2)
+    maxpercent.append(maxi_rounded)
+    a = a+1
+    if a == len(maxtimes):
+        break
 # Print the collected formatted times
-print("You started your best study sessions at these times and in these locations:")
+print("The modal shows that these were the most engaging and least stress inducing study sessions and they took place at these times in these locations:")
 time_place = 0
 inde = 0
 while time_place != len(formatted_times):
-    print(formatted_times[inde],' in ',Location_interpret_min[inde],'and you only wasted ',lowest_total_waste,'out of the total ',mintimes[inde],'minute(s)')
+    print('On',date_words_2[inde],' at ',formatted_times[inde],' in ',Location_interpret_min[inde],'and you only wasted ',lowest_total_waste,'out of the total ',mintimes[inde],'minute(s)')
     inde = inde+1
     time_place = time_place+1
 
-print('I recommend you study in any of these locations at these time where possible for optimal results')
+print('To optimise your future study sessions to maximise engagement and minimise stress, my model recommends your have your study sessions in these locations')
 print('\n')
-print("You started your worst study sessions at these times and in these locations:")
+print("The model shows that these were the most stress inducing study sessions and they took place at these times in these locations:")
 time_place_max = 0
 inde_max = 0
 while time_place_max != len(formatted_times_max):
-    print(formatted_times_max[inde_max],' in ',Location_interpret_max[inde_max],'and you wasted ',max_total_waste,'out of the total of ',maxtimes[inde_max],'minute(s)')
+    print('On',date_words[inde_max],' at ',formatted_times_max[inde_max],' in ',Location_interpret_max[inde_max],'and you wasted ',max_total_waste,'out of the total of ',maxtimes[inde_max],'minute(s)')
     inde_max = inde_max+1
     time_place_max = time_place_max+1
 
-print('I recommend you avoid studying in any of these locations at these time where possible for optimal results')
+print('The model recommends for the future that you avoid these locations in order to minimise stress induction and maximise engagement while studying because in these locations you wasted:')
+b = 0
+for y in maxpercent:
+    print(maxpercent[b],'% of your time studying in ',Location_interpret_max[b])
+    b=b+1
+    if b == len(maxpercent):
+        break
+    
+#print(totaltime)
+#print(wasteminutes)
 
-'''
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -285,4 +366,3 @@ ax.set_title("The ratio of wasted time in regards to total time in minutes spent
 ax.legend(loc="upper right")
 
 plt.show()
-'''
